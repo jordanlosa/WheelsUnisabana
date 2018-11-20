56f9d299c2f70ab6bab600cdb4c07cbd4ff04876
juanrivadeneniradelgado <juanrivadeneniradelgado@172.17.163.1>
2018-11-20 10:33:28 -0500

    crear branch

--- endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
+++ endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Facade {
-
+	/*riva*/
 	ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 	HashMap<Long,String> sesiones = new HashMap<Long,String>();
 	ArrayList<Usuario> usuarios= new ArrayList<Usuario>(); 
