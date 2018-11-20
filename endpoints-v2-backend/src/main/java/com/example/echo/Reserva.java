5e6081631315bbf40e5e4e47e33c575644d27306
juanrivadeneniradelgado <juanrivadeneniradelgado@172.17.163.1>
2018-11-20 09:50:16 -0500

    intial

---
+++ endpoints-v2-backend/src/main/java/com/example/echo/Reserva.java
@@ -0,0 +1,54 @@
+package main.java.com.example.echo;
+
+public class Reserva {
+
+ 	Usuario Pasajero;
+ 	String Ruta;
+	Double PuntoEncuentrox;
+	Double PuntoEncuentroy;
+	int Valor;
+	
+	public Reserva(Usuario pasajero, String ruta, Double puntoEncuentrox, Double puntoEncuentroy, int valor) {		
+		Pasajero = pasajero;
+		Ruta = ruta;
+		PuntoEncuentrox = puntoEncuentrox;
+		PuntoEncuentroy = puntoEncuentroy;
+		Valor = valor;
+	}
+	
+	public Reserva() {
+		
+	}
+
+	public Usuario getPasajero() {
+		return Pasajero;
+	}
+	public void setPasajero(Usuario pasajero) {
+		Pasajero = pasajero;
+	}
+	public String getRuta() {
+		return Ruta;
+	}
+	public void setRuta(String ruta) {
+		Ruta = ruta;
+	}
+	public Double getPuntoEncuentrox() {
+		return PuntoEncuentrox;
+	}
+	public void setPuntoEncuentrox(Double puntoEncuentrox) {
+		PuntoEncuentrox = puntoEncuentrox;
+	}
+	public Double getPuntoEncuentroy() {
+		return PuntoEncuentroy;
+	}
+	public void setPuntoEncuentroy(Double puntoEncuentroy) {
+		PuntoEncuentroy = puntoEncuentroy;
+	}
+	public int getValor() {
+		return Valor;
+	}
+	public void setValor(int valor) {
+		Valor = valor;
+	}
+	
+}
