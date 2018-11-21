2f69647a6f13b40f96f4978d99d14f2b4b8f2d8b
juanrivadeneniradelgado <juanrivadeneniradelgado@172.17.163.1>
2018-11-21 14:15:53 -0500

    a un tiro

--- WheelsUnisabana-riva/endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
+++ WheelsUnisabana-riva/endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
@@ -3,6 +3,36 @@ package main.java.com.example.echo;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiIssuer;
+import com.google.api.server.spi.config.ApiIssuerAudience;
+import com.google.api.server.spi.config.ApiNamespace;
+
+@Api(
+	    name = "facade",
+	    version = "v1",
+	    namespace =
+	    @ApiNamespace(
+	        ownerDomain = "echo.example.com",
+	        ownerName = "echo.example.com",
+	        packagePath = ""
+	    ),
+	    // [START_EXCLUDE]
+	    issuers = {
+	        @ApiIssuer(
+	            name = "firebase",
+	            issuer = "https://securetoken.google.com/wheels20181019",
+	            jwksUri =
+	                "https://www.googleapis.com/service_accounts/v1/metadata/x509/securetoken@system"
+	                    + ".gserviceaccount.com"
+	        )
+	    },
+	    		issuerAudiences = {
+	    		        @ApiIssuerAudience(name = "firebase", audiences = "el-que-yo-quiera")
+	    		    }
+	// [END_EXCLUDE]
+	)
+
 public class Facade 
 {
 	/*riva hecho*/
