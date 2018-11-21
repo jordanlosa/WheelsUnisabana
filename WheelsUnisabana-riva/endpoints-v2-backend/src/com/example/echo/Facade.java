45f2bb7957baea1cba61e8af1882fe8d047639b3
juanrivadeneniradelgado <juanrivadeneniradelgado@172.17.163.1>
2018-11-21 13:32:12 -0500

    hecho

--- WheelsUnisabana-riva/endpoints-v2-backend/src/com/example/echo/Facade.java
+++ WheelsUnisabana-riva/endpoints-v2-backend/src/com/example/echo/Facade.java
@@ -5,11 +5,10 @@ import java.util.HashMap;
 
 public class Facade 
 {
-	/*riva*/
+	/*riva hecho*/
 	ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 	HashMap<Long,String> sesiones = new HashMap<Long,String>();
 	ArrayList<Usuario> usuarios= new ArrayList<Usuario>(); 
-	
-	
+
 
 }
