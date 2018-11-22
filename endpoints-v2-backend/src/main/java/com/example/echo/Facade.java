02759569def4410eca3075c6e0f0a618a4c1e0ee
JuanDiego1589 <juan.d.alvarado15@hotmail.com>
2018-11-22 12:39:43 -0500

    Modificar

--- endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
+++ endpoints-v2-backend/src/main/java/com/example/echo/Facade.java
@@ -1,15 +1,36 @@
-package com.example.echo;
-
+package main.java.com.example.echo;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+
 public class Facade {
 
+	/*juanD */
 	ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 	HashMap<Long,String> sesiones = new HashMap<Long,String>();
 	ArrayList<Usuario> usuarios= new ArrayList<Usuario>(); 
 	
+	@ApiMethod(name="modificar", path="reservamodif")
+	public void modificarReserva (@Named ("n")Integer IDreserva, @Named ("p")String Pasajero, @Named ("x")Double PuntoX, @Named ("y") Double PuntoY, @Named ("v")Integer Valor   )
+	{
+		for (int i=0; i < reservas.size(); i++) {
+			
+			if(reservas.get(i).getPasajero().getNombre().equals(Pasajero)) {
+				
+				reservas.get(i).setPuntoEncuentrox(PuntoX);
+				reservas.get(i).setPuntoEncuentroy(PuntoY);
+				reservas.get(i).setValor(Valor);
+				
+			}
+			
+			
+		}
+		
+		
+	}
 	
 	
 }
